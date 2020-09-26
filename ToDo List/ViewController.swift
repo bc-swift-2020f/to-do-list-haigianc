ffb5352cc9e876a295782fc19c9b004ffeab67f9
Claudine Haigian <haigianc@bc.edu>
2020-09-26 14:12:57 -0400

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Claudine Haigian on 9/26/20.
+//  Copyright © 2020 Claudine Haigian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
