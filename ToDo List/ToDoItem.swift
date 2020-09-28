35d4075d5acc9ff00ac85893412ed1af707ac12e
Claudine Haigian <haigianc@bc.edu>
2020-09-28 15:35:28 -0400

    Week 5 Work: through 5.9

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Claudine Haigian on 9/28/20.
+//  Copyright © 2020 Claudine Haigian. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem: Codable {
+    var name: String
+    var date: Date
+    var notes: String
+    var reminderSet: Bool
+}
